f157eba752d38521c55459e3dab59242e681ce06
Shaun Maxwell <maxwell.shaun.0341@gmail.com>
2023-12-06 16:47:03 -0500

    Pushing

--- vue/src/components/VisitChart.vue
+++ vue/src/components/VisitChart.vue
@@ -1,79 +1,33 @@
 <template>
-  <span>
-    <button v-on:click="fetchVisitsByUserId"> Click me first</button>
-    <button v-on:click="populateChartData"> Click me second </button>
-  </span>
-  <Bar :data="chartData" />
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
-import CheckInOutService from '../services/CheckInOutService';
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
   name: 'BarChart',
   components: { Bar },
-  data() {
-    return {
-      userId: "",
-      durationValue: [],
-      chartData: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: []
-          }
-        ]
-      }
-    };
-  },
-  methods: {
-    setUserId() {
-      this.userId = this.$store.getters.getUserId;
-    },
-    fetchVisitsByUserId() {
-      this.setUserId();
-      CheckInOutService.getVisitsByUserId(this.userId)
-        .then(response => {
-          if (response.status === 200) {
-            this.durationValue = response.data
-          }
-          console.log(this.durationValue)
-        })
-    },
-    populateChartData() {
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
 
-        this.chartData.labels= this.durationValue;
-      
+    try {
+      const { visits } = await fetch('http://localhost:9000/visits/1')
+      this.chartdata = visits
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
     }
-
-    // async fetchVisitsByUserId() {
-    //   try {
-    //     await this.setUserId(); 
-    //     const response = await CheckInOutService.getVisitsByUserId(this.userId);
-    //     this.durationValue = response.data.map((visit) => visit.duration);
-    //     console.log(this.durationValue);
-
-
-    //     this.chartData.datasets[0].data = [];
-
-    //     this.durationValue.forEach(element => {
-    //       this.chartData.datasets[0].data.push(element);
-    //     });
-    //   } catch (error) {
-    //     console.error('Error fetching data:', error);
-
-    //   }
-    // },
-  },
-  mounted() {
-    this.setUserId
-    this.fetchVisitsByUserId();
-  },
-};
+  }
+}
 </script>
