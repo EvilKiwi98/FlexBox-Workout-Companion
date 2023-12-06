a5d9683fe140dbf671d334ca5d14eab35a37ae82
Aaron Loskota <aloskota@gmail.com>
2023-12-06 13:47:04 -0500

    table trying

--- vue/src/components/VisitChart.vue
+++ vue/src/components/VisitChart.vue
@@ -1,31 +1,58 @@
 <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+  <Bar :data="chartData" />
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+import CheckInOutService from '../services/CheckInOutService';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
   name: 'BarChart',
   components: { Bar },
   data() {
     return {
+      userId: "", 
+      durationValue: [],
       chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
+        labels: ['January', 'February', 'March','April','May','June'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: []
+          }
+        ]
       }
-    }
-  }
-}
+    };
+  },
+  methods: {
+    setUserId() {
+      this.userId = this.$store.getters.getUserId;
+    },
+    async fetchVisitsByUserId() {
+      try {
+        await this.setUserId(); 
+        const response = await CheckInOutService.getVisitsByUserId(this.userId);
+        this.durationValue = response.data.map((visit) => visit.duration);
+        console.log(this.durationValue);
+
+        
+        this.chartData.datasets[0].data = [];
+
+        this.durationValue.forEach(element => {
+          this.chartData.datasets[0].data.push(element);
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        
+      }
+    },
+  },
+  mounted() {
+    this.fetchVisitsByUserId();
+  },
+};
 </script>
-  <style></style>
