38e051d4482c04dd7286c27dd08c146ebfd626c7
Christian Hong <christian.hong.0902@gmail.com>
2023-12-07 15:01:37 -0500

    Changes

--- vue/src/components/VisitChart.vue
+++ vue/src/components/VisitChart.vue
@@ -1,225 +1,225 @@
-<template>
-  <div>
-    <Bar :data="chartData" :key="chartKey" />
-    <div class="button-container">
-      <button @click="viewBy('days')">View by Days</button>
-      <button @click="viewBy('weeks')">View Past Week Time Spent</button>
-      <button @click="viewBy('months')">View Average Time Spent by Month</button>
+  <template>
+    <div>
+      <Bar :data="chartData" :key="chartKey" />
+      <div class="button-container">
+        <button @click="viewBy('days')">View by Days</button>
+        <button @click="viewBy('weeks')">View Past Week Time Spent</button>
+        <button @click="viewBy('months')">View Average Time Spent by Month</button>
+      </div>
     </div>
-  </div>
-</template>
+  </template>
 
-<script>
-import { Bar } from 'vue-chartjs';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
-import CheckInOutService from '../services/CheckInOutService';
+  <script>
+  import { Bar } from 'vue-chartjs';
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+  import CheckInOutService from '../services/CheckInOutService';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      userId: '',
-      chartData: {
-        labels: [],
-        datasets: [
-          {
-            label: 'Minutes',
-            backgroundColor: '#3498db',
-            data: [],
-          },
-        ],
+  export default {
+    name: 'BarChart',
+    components: { Bar },
+    data() {
+      return {
+        userId: '',
+        chartData: {
+          labels: [],
+          datasets: [
+            {
+              label: 'Minutes',
+              backgroundColor: '#3498db',
+              data: [],
+            },
+          ],
+        },
+        chartKey: 0,
+        viewMode: 'days', // Default view mode
+      };
+    },
+    computed: {
+      getUserId() {
+        return this.$store.getters.getUserId;
       },
-      chartKey: 0,
-      viewMode: 'days', // Default view mode
-    };
-  },
-  computed: {
-    getUserId() {
-      return this.$store.getters.getUserId;
     },
-  },
-  methods: {
-    async fetchData() {
-  try {
-    this.userId = this.getUserId;
-    const response = await CheckInOutService.getVisitsByUserId(this.userId);
+    methods: {
+      async fetchData() {
+    try {
+      this.userId = this.getUserId;
+      const response = await CheckInOutService.getVisitsByUserId(this.userId);
 
-    console.log('Response:', response);
+      console.log('Response:', response);
 
-    // Assuming the actual visits are in the 'data' property
-    const visits = response.data;
+      // Assuming the actual visits are in the 'data' property
+      const visits = response.data;
 
-    if (Array.isArray(visits)) {
-      if (this.viewMode === 'days') {
-        // Group visits by day
-        const visitsByDay = visits.reduce((result, visit) => {
-          const formattedDate = formatDate(visit.checkInTime);
-          if (!result[formattedDate]) {
-            result[formattedDate] = { totalDuration: 0, visitCount: 0 };
-          }
-          result[formattedDate].totalDuration += visit.duration;
-          result[formattedDate].visitCount += 1;
-          return result;
-        }, {});
+      if (Array.isArray(visits)) {
+        if (this.viewMode === 'days') {
+          // Group visits by day
+          const visitsByDay = visits.reduce((result, visit) => {
+            const formattedDate = formatDate(visit.checkInTime);
+            if (!result[formattedDate]) {
+              result[formattedDate] = { totalDuration: 0, visitCount: 0 };
+            }
+            result[formattedDate].totalDuration += visit.duration;
+            result[formattedDate].visitCount += 1;
+            return result;
+          }, {});
 
-        // Calculate average duration for each day
-        const averageDurations = Object.keys(visitsByDay).map((date) => {
-          const averageDuration =
-            visitsByDay[date].totalDuration / visitsByDay[date].visitCount;
-          return averageDuration.toFixed(2); // Round to 2 decimal places
-        });
+          // Calculate average duration for each day
+          const averageDurations = Object.keys(visitsByDay).map((date) => {
+            const averageDuration =
+              visitsByDay[date].totalDuration / visitsByDay[date].visitCount;
+            return averageDuration.toFixed(2); // Round to 2 decimal places
+          });
 
-        // Use formatted dates as labels
-        this.chartData.labels = Object.keys(visitsByDay);
-        this.chartData.datasets[0].data = averageDurations;
-        this.chartKey += 1;
-      } else if (this.viewMode === 'weeks') {
-        // Display daily time spent for the past week
-        const lastWeekStart = new Date();
-        lastWeekStart.setDate(lastWeekStart.getDate() - lastWeekStart.getDay() - 6); // Start from the first day of the past week
-        const lastWeekEnd = new Date();
-        lastWeekEnd.setDate(lastWeekStart.getDate() + 6); // End on the last day of the past week
+          // Use formatted dates as labels
+          this.chartData.labels = Object.keys(visitsByDay);
+          this.chartData.datasets[0].data = averageDurations;
+          this.chartKey += 1;
+        } else if (this.viewMode === 'weeks') {
+          // Display daily time spent for the past week
+          const lastWeekStart = new Date();
+          lastWeekStart.setDate(lastWeekStart.getDate() - lastWeekStart.getDay() - 6); // Start from the first day of the past week
+          const lastWeekEnd = new Date(lastWeekStart);
+          lastWeekEnd.setDate(lastWeekEnd.getDate() + 6); // End on the last day of the past week
 
-        // Filter visits for the past week
-        const visitsPastWeek = visits.filter((visit) => {
-          const visitDate = new Date(visit.checkInTime);
-          return visitDate >= lastWeekStart && visitDate <= lastWeekEnd;
-        });
+          // Filter visits for the past week
+          const visitsPastWeek = visits.filter((visit) => {
+            const visitDate = new Date(visit.checkInTime);
+            return visitDate >= lastWeekStart && visitDate <= lastWeekEnd;
+          });
 
-        // Group visits by day
-        const visitsByDay = visitsPastWeek.reduce((result, visit) => {
-          const formattedDate = formatDate(visit.checkInTime);
-          if (!result[formattedDate]) {
-            result[formattedDate] = { totalDuration: 0, visitCount: 0 };
-          }
-          result[formattedDate].totalDuration += visit.duration;
-          result[formattedDate].visitCount += 1;
-          return result;
-        }, {});
+          // Group visits by day
+          const visitsByDay = visitsPastWeek.reduce((result, visit) => {
+            const formattedDate = formatDate(visit.checkInTime);
+            if (!result[formattedDate]) {
+              result[formattedDate] = { totalDuration: 0, visitCount: 0 };
+            }
+            result[formattedDate].totalDuration += visit.duration;
+            result[formattedDate].visitCount += 1;
+            return result;
+          }, {});
 
-        // Calculate average duration for each day
-        const averageDurations = Object.keys(visitsByDay).map((date) => {
-          const averageDuration =
-            visitsByDay[date].totalDuration / visitsByDay[date].visitCount;
-          return averageDuration.toFixed(2); // Round to 2 decimal places
-        });
+          // Calculate average duration for each day
+          const averageDurations = Object.keys(visitsByDay).map((date) => {
+            const averageDuration =
+              visitsByDay[date].totalDuration / visitsByDay[date].visitCount;
+            return averageDuration.toFixed(2); // Round to 2 decimal places
+          });
 
-        // Use formatted dates as labels
-        this.chartData.labels = Object.keys(visitsByDay);
-        this.chartData.datasets[0].data = averageDurations;
-        this.chartKey += 1;
-      } else if (this.viewMode === 'months') {
-        // Group visits by month
-        const visitsByMonth = visits.reduce((result, visit) => {
-          const formattedMonth = formatMonth(visit.checkInTime);
-          if (!result[formattedMonth]) {
-            result[formattedMonth] = { totalDuration: 0, visitCount: 0 };
-          }
-          result[formattedMonth].totalDuration += visit.duration;
-          result[formattedMonth].visitCount += 1;
-          return result;
-        }, {});
+          // Use formatted dates as labels
+          this.chartData.labels = Object.keys(visitsByDay);
+          this.chartData.datasets[0].data = averageDurations;
+          this.chartKey += 1;
+        } else if (this.viewMode === 'months') {
+          // Group visits by month
+          const visitsByMonth = visits.reduce((result, visit) => {
+            const formattedMonth = formatMonth(visit.checkInTime);
+            if (!result[formattedMonth]) {
+              result[formattedMonth] = { totalDuration: 0, visitCount: 0 };
+            }
+            result[formattedMonth].totalDuration += visit.duration;
+            result[formattedMonth].visitCount += 1;
+            return result;
+          }, {});
 
-        // Calculate average duration for each month
-        const averageDurations = Object.keys(visitsByMonth).map((month) => {
-          const averageDuration =
-            visitsByMonth[month].totalDuration / visitsByMonth[month].visitCount;
-          return averageDuration.toFixed(2); // Round to 2 decimal places
-        });
+          // Calculate average duration for each month
+          const averageDurations = Object.keys(visitsByMonth).map((month) => {
+            const averageDuration =
+              visitsByMonth[month].totalDuration / visitsByMonth[month].visitCount;
+            return averageDuration.toFixed(2); // Round to 2 decimal places
+          });
 
-        // Assuming you have a function to format the month, similar to formatWeek
-        this.chartData.labels = Object.keys(visitsByMonth).map((month) => formatMonth(month));
-        this.chartData.datasets[0].data = averageDurations;
-        this.chartKey += 1;
-      }
-    } else {
-      console.error('Received data is not an array:', visits);
-    }
-  } catch (error) {
-    console.error('Error fetching visits:', error);
-  }
-},
-
-    // Method to switch between view modes
-    viewBy(mode) {
-      this.viewMode = mode;
-      this.fetchData();
-    },
-
-
-    populateChartData() {
-      // Additional logic if needed when populating chart data
-    },
-
-    getVisitsByWeekByUserId() {
-      this.isLoading = true;
-      CheckInOutService.getVisitsByWeekByUserId(this.$store.getters.getUserId).then(
-        (response) => {
-          this.visits = response.data;
+          // Assuming you have a function to format the month, similar to formatWeek
+          this.chartData.labels = Object.keys(visitsByMonth).map((month) => formatMonth(month));
+          this.chartData.datasets[0].data = averageDurations;
+          this.chartKey += 1;
         }
-      );
+      } else {
+        console.error('Received data is not an array:', visits);
+      }
+    } catch (error) {
+      console.error('Error fetching visits:', error);
+    }
+  },
+
+      // Method to switch between view modes
+      viewBy(mode) {
+        this.viewMode = mode;
+        this.fetchData();
+      },
+
+
+      populateChartData() {
+        // Additional logic if needed when populating chart data
+      },
+
+      getVisitsByWeekByUserId() {
+        this.isLoading = true;
+        CheckInOutService.getVisitsByWeekByUserId(this.$store.getters.getUserId).then(
+          (response) => {
+            this.visits = response.data;
+          }
+        );
+      },
     },
-  },
-  async mounted() {
-    // Fetch data when the component is mounted
-    await this.fetchData();
-  },
-};
+    async mounted() {
+      // Fetch data when the component is mounted
+      await this.fetchData();
+    },
+  };
 
-function formatDate(dateString) {
-  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-}
-// Function to get the week number for a given date
-function getWeekNumber(date) {
-  const d = new Date(date);
-  d.setHours(0, 0, 0, 0);
-  d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-  const yearStart = new Date(d.getFullYear(), 0, 1);
-  const weekNumber = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-  return weekNumber;
-}
+  function formatDate(dateString) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  }
+  // Function to get the week number for a given date
+  function getWeekNumber(date) {
+    const d = new Date(date);
+    d.setHours(0, 0, 0, 0);
+    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+    const yearStart = new Date(d.getFullYear(), 0, 1);
+    const weekNumber = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    return weekNumber;
+  }
 
-function formatWeek(week) {
-  return `Week ${week}`;
-}
+  function formatWeek(week) {
+    return `Week ${week}`;
+  }
 
-function formatMonth(dateString) {
-  const options = { year: 'numeric', month: 'long' };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-}
-</script>
+  function formatMonth(dateString) {
+    const options = { year: 'numeric', month: 'long' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  }
+  </script>
 
 
-<style scoped>
-div {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+  <style scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-.button-container {
-  margin-top: 20px;
-  display: flex;
-  flex-direction: row; /* Set the direction to row */
-}
+  .button-container {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row; /* Set the direction to row */
+  }
 
-button {
-  margin: 5px;
-  padding: 10px;
-  font-size: 16px;
-  background-color: #6ad5e5;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s;
-}
+  button {
+    margin: 5px;
+    padding: 10px;
+    font-size: 16px;
+    background-color: #6ad5e5;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
 
-button:hover {
-  background-color: #2980b9;
-}
-</style>
+  button:hover {
+    background-color: #2980b9;
+  }
+  </style>
--- vue/src/views/ProfileView.vue
+++ vue/src/views/ProfileView.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="profile-view">
-    <exercise-list />
-    <visit-list></visit-list>
-    <visit-chart></visit-chart>
+    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
+    style="min-height: 600px; background-image: url('../assets/images/FitnessBackground.jpg'); background-size: cover; background-position: center top;">
+      <span class="mask bg-gradient-default opacity-8"></span>
+      <div class="container-fluid d-flex align-items-center">
+        <div class="row">
+          <div class="col-lg-7 col-md-10">
+            <h1 class="display-2 text-white">Hello</h1>
+            <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-container">
+      <div class="exercise-container">
+        <exercise-list />
+      </div>
+      <div class="visit-container">
+        <visit-list></visit-list>
+      </div>
+    </div>
+    <div class="chart-container">
+      <visit-chart></visit-chart>
+    </div>
   </div>
 </template>
 
@@ -13,7 +33,7 @@ import VisitList from '../components/VisitList.vue';
 import VisitChart from '../components/VisitChart.vue';
 
 export default {
-  components:{
+  components: {
     ExerciseList,
     VisitList,
     VisitChart
@@ -24,8 +44,72 @@ export default {
 
 <style scoped>
 .profile-view {
-  max-width: 800px;
+  width: 100%;
+  flex-direction: column;
+  max-width: 1200px;
   margin: 0 auto;
   padding: 20px;
+  background: linear-gradient(to bottom, rgba(201, 200, 254, 0.8) 33%, rgba(255, 255, 255, 0.8) 33%);
+  background-size: cover;
+  background-position: center;
+}
+
+.header {
+  min-height: 600px;
+  display: flex;
+  align-items: center;
+  background-size: cover;
+  background-position: center top;
+}
+
+.main-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.exercise-container {
+  flex: 2;
+  margin-right: 20px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.visit-container {
+  flex: 1;
+  margin-left: 20px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.chart-container {
+  width: 100%;
+  margin-top: 20px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.visit-chart {
+  width: 100%;
+}
+
+.col-md-10,
+.col-lg-7 {
+  max-width: 58.33333%;
+  flex: 0 0 58.33333%;
+}
+
+.col-md-10,
+.col-lg-7 {
+  position: relative;
+  width: 100%;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
 }
 </style>
