427506bbad6483a3ec202cf7aba1b8d9b8f15946
Aaron Loskota <aloskota@gmail.com>
2023-12-07 09:21:33 -0500

    chart fixes

--- vue/src/components/VisitChart.vue
+++ vue/src/components/VisitChart.vue
@@ -1,33 +1,71 @@
 <template>
-  <div class="container">
-    <Bar v-if="loaded" :data="chartData" />
+  <div>
+    <Bar :data="chartData" :key="chartKey" />
   </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+import CheckInOutService from '../services/CheckInOutService';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
   name: 'BarChart',
   components: { Bar },
-  data: () => ({
-    loaded: false,
-    chartData: null
-  }),
-  async mounted () {
-    this.loaded = false
+  data() {
+    return {
+      userId: "",
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Visits',
+            backgroundColor: '#f87979',
+            data: []
+          }
+        ]
+      },
+      chartKey: 0,
+    };
+  },
+  computed: {
+    getUserId() {
+      return this.$store.getters.getUserId;
+    },
+  },
+  methods: {
+    async fetchData() {
+      try {
+        this.userId = this.getUserId;
+        const response = await CheckInOutService.getVisitsByUserId(this.userId);
 
-    try {
-      const { visits } = await fetch('http://localhost:9000/visits/1')
-      this.chartdata = visits
+        console.log('Response:', response);
 
-      this.loaded = true
-    } catch (e) {
-      console.error(e)
-    }
-  }
-}
+        // Assuming the actual visits are in the 'data' property
+        const visits = response.data;
+
+        if (Array.isArray(visits)) {
+          this.chartData.labels = visits.map(visit => visit.checkInTime);
+          this.chartData.datasets[0].data = visits.map(visit => visit.duration);
+          this.chartKey += 1;
+        } else {
+          console.error('Received data is not an array:', visits);
+        }
+      } catch (error) {
+        console.error('Error fetching visits:', error);
+      }
+    },
+
+
+    populateChartData() {
+      // Additional logic if needed when populating chart data
+    },
+  },
+  async mounted() {
+    // Fetch data when the component is mounted
+    await this.fetchData();
+  },
+};
 </script>
