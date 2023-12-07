f3c9e19055cdc7791bd631b58962be41788cadf0
Aaron Loskota <aloskota@gmail.com>
2023-12-07 15:16:56 -0500

    new component

---
+++ vue/src/components/ExerciseButtons.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="exercise-buttons">
+      <button
+        v-for="exerciseOption in exerciseOptions"
+        :key="exerciseOption.id"
+        @click="selectExercise(exerciseOption)"
+      >
+        <img
+          :src="`../assets/${exerciseOption.name.replace(/\s+/g, '-').toLowerCase()}.png`"
+          alt="Exercise Image"
+          class="button-icon"
+        />
+        {{ exerciseOption.name }}
+      </button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['selectExercise'],
+    data() {
+      return {
+        exerciseOptions: [
+          { id: 1, name: "Bench Press", mode: "reps" },
+          // ... (Other exercise options)
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Style the button container and icon as needed */
+  .exercise-buttons {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  
+  .button-icon {
+    width: auto; /* Let the width adjust automatically */
+    height: 60px; /* Set a fixed height */
+    margin-right: 10px;
+  }
+  </style>
