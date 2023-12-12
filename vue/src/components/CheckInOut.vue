fcadd34054b129dc2a9a1a079e5dbc5a3c0dee71
Shaun Maxwell <maxwell.shaun.0341@gmail.com>
2023-12-12 16:01:40 -0500

    Pulling

--- vue/src/components/CheckInOut.vue
+++ vue/src/components/CheckInOut.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="main">
-    <button
-      @click="toggleCheckInOut"
-      :class="{ 'check-btn-in': !isCheckedIn, 'check-btn-out': isCheckedIn }"
-    >
+    <button @click="toggleCheckInOut" :class="{ 'check-btn-in': !isCheckedIn, 'check-btn-out': isCheckedIn }">
       {{ checkInOutLabel }}
     </button>
-    <div v-if="isCheckedIn" class="status-message success" id="checked-in">
-      <i class="fas fa-check-circle"></i>
-      <span class="check-time"
-        >Checked in at:
-        {{
-          CheckInOut.checkInTime.toLocaleString("en-US", { timeZone: "EST" })
-        }}</span
-      >
-    </div>
-    <div v-if="!isCheckedIn" class="status-message" id="duration">
-      <i class="fas fa-info-circle"></i> Checked out at: {{ checkOutTime }}
-      <span>Duration: {{ visitDuration }} {{ visitDuration === 1 ? 'minute' : 'minutes' }}</span>
+    <div v-show="showAll" id="messages">
+      <div v-if="isCheckedIn" class="status-message success" id="checked-in">
+        <i class="fas fa-check-circle"></i>
+        <span class="check-time">Checked in at:
+          {{
+            CheckInOut.checkInTime.toLocaleString("en-US", { timeZone: "EST" })
+          }}</span>
+      </div>
+      <div v-if="!isCheckedIn" class="status-message" id="duration">
+        <i class="fas fa-info-circle"></i> Checked out at: {{ checkOutTime }}
+        <span>Duration: {{ visitDuration }} {{ visitDuration === 1 ? 'minute' : 'minutes' }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +24,7 @@ import CheckInOutService from "../services/CheckInOutService";
 export default {
   data() {
     return {
+      showAll:false,
       currentTime: null,
       checkOutTime: null,
       isCheckedIn: false,
@@ -63,7 +61,7 @@ export default {
       const checkInTime = new Date(this.CheckInOut.checkInTime);
       const checkOutTime = new Date(this.CheckInOut.checkOutTime);
       const durationInMillis = checkOutTime - checkInTime;
-      const durationInMinutes = Math.floor(durationInMillis /(1000*60));
+      const durationInMinutes = Math.floor(durationInMillis / (1000 * 60));
       this.CheckInOut.duration = `${durationInMinutes}`;
     },
     sendCheckIn() {
@@ -76,6 +74,7 @@ export default {
             console.log(response.data);
             this.CheckInOut.userVisitId = response.data.userVisitId;
             this.isCheckedIn = true;
+            this.showAll=true;
           }
         });
     },
@@ -84,21 +83,21 @@ export default {
         this.setCheckOutTime(),
         this.setUserId(),
         this.calculateDuration();
-        this.visitDuration = this.CheckInOut.duration;
-        CheckInOutService.checkOut(this.CheckInOut).then((response) => {
-          if (response.status === 200) {
-            //we are successful
-            console.log(response.data);
-            this.isCheckedIn = false;
-            this.CheckInOut.checkOutTime = new Date();
-            this.checkOutTime = this.CheckInOut.checkOutTime.toLocaleString(
-              "en-US",
-              { timeZone: "EST" }
-            );
-            
-            this.CheckInOut = {};
-          }
-        });
+      this.visitDuration = this.CheckInOut.duration;
+      CheckInOutService.checkOut(this.CheckInOut).then((response) => {
+        if (response.status === 200) {
+          //we are successful
+          console.log(response.data);
+          this.isCheckedIn = false;
+          this.CheckInOut.checkOutTime = new Date();
+          this.checkOutTime = this.CheckInOut.checkOutTime.toLocaleString(
+            "en-US",
+            { timeZone: "EST" }
+          );
+
+          this.CheckInOut = {};
+        }
+      });
     },
     toggleCheckInOut() {
       if (this.isCheckedIn) {
@@ -113,62 +112,66 @@ export default {
 
 
 <style scoped>
-.main{
+.main {
   display:grid;
-  grid-template-columns:1fr 1fr;
-  grid-column-gap:5px;
-  grid-row-gap:5px;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
   grid-template-areas:
-  "button checked-in"
-  "button duration";
-  width:30%;
+  "the-button message"
+  "the-button message";
 
 }
-
-#checked-in{
-  grid-area:"checked-in"
+#messages{
+  grid-area:"message"
 }
-#duration{
-  grid-area:"duration"
-}
-
 .check-btn-in,
 .check-btn-out {
   font-family: 'Exo 2', sans-serif;
-  background-color: #2ecc71; /* Green color for Check In */
+  background-color: #2ecc71;
+  /* Green color for Check In */
   color: #fff;
-  padding: 25px 50px; /* Increased padding for a bigger button */
-  font-size: 20px; /* Increased font size */
+  padding: 25px 50px;
+  /* Increased padding for a bigger button */
+  font-size: 20px;
+  /* Increased font size */
   cursor: pointer;
   border: none;
   border-radius: 12px;
   transition: background-color 0.3s ease, box-shadow 0.3s ease;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); /* Subtle box shadow for a modern look */
-  grid-area: "button";
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  /* Subtle box shadow for a modern look */
+  grid-area: "the-button";
+  width:200px;
+
 }
 
 .check-btn-in:hover {
-  background-color: #27ae60; /* Darker green on hover */
+  background-color: #27ae60;
+  /* Darker green on hover */
 }
 
 .check-btn-out {
-  background-color: #e74c3c; /* Red color for Check Out */
+  background-color: #e74c3c;
+  /* Red color for Check Out */
 }
 
 .check-btn-out:hover {
-  background-color: #c0392b; /* Darker red on hover */
+  background-color: #c0392b;
+  /* Darker red on hover */
 }
 
 /* Rest of the styles */
 .status-message {
   margin-top: 20px;
   font-size: 16px;
-  text-align: center;
+  text-align: right;
   padding: 10px;
 }
 
 .check-time {
-  color: black; /* Set the text color to black */
+  color: black;
+  /* Set the text color to black */
 }
 
 .success {
@@ -186,5 +189,4 @@ export default {
 
 .fa-info-circle {
   color: #3498db;
-}
-</style>
+}</style>
