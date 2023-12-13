8fb39249990a67db1438d068f3193e90ad67bc6d
Aidan Hutchinson <hutcha771@gmail.com>
2023-12-13 13:09:09 -0500

    Update

--- vue/src/views/QRCodeScannerView.vue
+++ vue/src/views/QRCodeScannerView.vue
@@ -1,55 +1,50 @@
 <template>
-  <div>
-    <div class="qrcode-container">
-      <StreamBarcodeReader
-        @decode="onDecode"
-        @loaded="onLoaded"
-      ></StreamBarcodeReader>
+    <div>
+      <div class="qrcode-container">
+        <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+      </div>
     </div>
-  </div>
-</template>
-
-<script>
-import { StreamBarcodeReader } from "vue-barcode-reader";
-
-export default {
-  components: {
-    StreamBarcodeReader,
-  },
-  data() {
-    return {
-      decodedResult: null,
-    };
-  },
-  navigateToScan() {
-    this.$router.push({ name: "scan" });
-  },
-
-  onDecode(result) {
-    console.log(result);
-    this.decodedResult = result;
-    if (this.decodedResult) {
-      window.location.href = this.decodedResult;
-    }
-  },
-  redirectToWebsite() {
-    // Redirect to another website based on the decoded result
-    if (this.decodedResult) {
-      try {
-        window.location.href = this.decodedResult;
-      } catch (error) {
-        console.error("Error redirecting:", error);
-      }
-    }
-  },
-};
-</script>
-
-<style>
-.qrcode-container {
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-</style>
+  </template>
+  <script>
+  import { StreamBarcodeReader } from "vue-barcode-reader";
+  export default {
+    components: {
+      StreamBarcodeReader,
+    },
+    data() {
+      return {
+        decodedResult: null,
+      };
+    },
+    methods: {
+      onDecode(result) {
+        console.log(result);
+        this.decodedResult = result;
+        // Check if the decoded result is a valid URL
+        if (this.isUrl(this.decodedResult)) {
+          // Redirect to the website
+          window.location.href = this.decodedResult;
+        } else {
+          // If not a URL, navigate to a different route using Vue Router
+          this.$router.push({ name: "scan", query: { result } });
+        }
+      },
+      onLoaded() {
+        console.log(`Ready to start scanning barcodes`);
+      },
+      isUrl(value) {
+        // Simple URL validation, you can enhance it based on your requirements
+        const urlPattern = /^(http|https):\/\/.*$/;
+        return urlPattern.test(value);
+      },
+    },
+  };
+  </script>
+  <style>
+  .qrcode-container {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  </style>
